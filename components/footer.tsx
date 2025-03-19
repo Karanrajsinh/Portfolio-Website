c2a805605da597e41fd5907293ce796d7413afab
Karanrajsinh <vaghelakaranraj54@gmail.com>
2025-03-19 23:22:31 +0530

    Footer Typo Fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -13,7 +13,7 @@ export function Footer() {
         <footer className="py-8 px-6">
             <div className="max-w-6xl mx-auto flex flex-col md:flex-row justify-between items-center gap-4">
                 <p className="text-sm text-muted-foreground">
-                    © {new Date().getFullYear()} Vachela Karanrajsinh. All rights reserved.
+                    © {new Date().getFullYear()}  Karanrajsinh Vaghela. All rights reserved.
                 </p>
 
                 <div className="flex items-center">
